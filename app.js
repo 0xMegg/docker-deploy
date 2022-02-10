636c687a89bcf495deae7d1480583ee54fbc1ef6
finero-d <finero91@gmail.com>
2022-02-10 15:28:27 +0900

    discord

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello Discord'));
 app.listen(port, () => { console.log(`My REST API running on ${port}`); })
