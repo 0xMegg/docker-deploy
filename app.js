addf81899f4d25b165c50541825c35fa565177d0
김준엽 <mero-dsc@gimjun-yeob-ui-MacBookPro.local>
2022-09-07 12:41:57 +0900

    test02

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
 const port = process.env.PORT || 8080;
-app.get('/', (req, res) => res.send('Hello Friday'));
-app.listen(port, () => { console.log(`My REST API running on ${port}`); })
+
+app.get("/", (req, res) => res.send("Hello Friday"));
+app.listen(port, () => {
+  console.log(`My REST API running on ${port}`);
+});
